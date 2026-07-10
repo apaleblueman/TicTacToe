5db06cac3fba2dcef40d96050b4215bf58b6f6aa
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-07-10 15:07:27 +0530

    FIX: winning diagonal logic for both diagonals

--- JS/script.js
+++ JS/script.js
@@ -34,44 +34,75 @@ function mark(marker, row, col){
 }
 
 function checkWinner(gameBoard,row, col,marker){
-    let winner;
-    for(let chks=0;chks<3;chks++){
-    let flag = 0;
+    let winner,flag;
+    for (let chks = 0; chks < 3; chks++) {
+    flag = 0;
     //check rows
-    for(let c=0;c<3;c++){
-        if(gameBoard[row][c] == marker){
+    for (let c = 0; c < 3; c++) {
+        if (gameBoard[row][c] == marker) {
             flag++;
         }
     }
-    if(flag==3){
+    if (flag == 3) {
         winner = marker;
-    }else{ flag = 0;}
-    
+
+    } else { flag = 0; }
+
     //check cols
-    for(let r=0;r<3;r++){
-        if(gameBoard[r][col] == marker){
+    for (let r = 0; r < 3; r++) {
+        if (gameBoard[r][col] == marker) {
             flag++;
         }
     }
-    if(flag==3){
+    if (flag == 3) {
         winner = marker;
-    }else{ flag = 0;}
-    
+
+    } else { flag = 0; }
+
     //check diagnols
-    for(let r=0;r<3;r++){
-        if(gameBoard[r][r] == marker){
+    //top-left
+    for (let r = 0; r < 3; r++) {
+        if (gameBoard[r][r] == marker) {
             flag++;
         }
     }
-    if(flag==3){
+    if (flag == 3) {
         winner = marker;
-    }else{ flag = 0;}
-    
+
+    } else { flag = 0; }
+    //top-right
+    for (let r = 0; r < 3; r++) {
+        if (gameBoard[r][2 - r] == marker) {
+            flag++;
+        }
     }
-    
-    console.log("Winner is" + winner);   
+    if (flag == 3) {
+        winner = marker;
+
+    } else { flag = 0; }
+}
+
+if(flag!=0){console.log("Winner is" + winner);}
+else{console.log("draw");}
 }
 initializeGame();
-mark(0,0,0);
-mark(0,1,1);
-mark(0,2,2);
+//draw scenario
+// mark(1, 0, 0)  // X at top-left
+// mark(0, 0, 1)  // O at top-middle
+// mark(1, 0, 2)  // X at top-right
+// mark(0, 1, 1)  // O at center
+// mark(1, 1, 0)  // X at middle-left
+// mark(0, 1, 2)  // O at middle-right
+// mark(1, 2, 1)  // X at bottom-middle
+// mark(0, 2, 0)  // O at bottom-left
+// mark(1, 2, 2)  // X at bottom-right
+//x winner scenario
+mark(1, 0, 0)  // X at top-left
+mark(0, 0, 1)  // O at top-middle
+mark(1, 1, 1)  // X at center
+mark(0, 1, 0)  // O at middle-left
+mark(1, 0, 2)  // X at top-right
+mark(0, 2, 2)  // O at bottom-right
+mark(1, 2, 0)  // X at bottom-left
+mark(0, 1, 2)  // O at middle-right
+mark(1, 2, 1)  // X at bottom-middle - X wins!
