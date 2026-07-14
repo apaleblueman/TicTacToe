49fe9588ad17e11f48a36c708c51df4a6d436ea7
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-07-14 15:59:24 +0530

    FIX: no return after winner found issue in checkWinner function

--- JS/script.js
+++ JS/script.js
@@ -8,7 +8,7 @@ function initializeGame(){
 function showGameBoard(){
         for(let row=0;row<gameBoard.length;row++){
             for(let col=0;col<gameBoard.length;col++){
-                //console.log(gameBoard[row][col]);
+                // console.log(gameBoard[row][col]);
                 process.stdout.write(gameBoard[row][col]);
             }
             process.stdout.write('\n');
@@ -27,11 +27,12 @@ function mark(marker, row, col){
         gameBoard[row][col] = marker;
         used_cells++;
         showGameBoard(gameBoard);
-        checkWinner(gameBoard,row, col, marker, used_cells);   
+        winner = checkWinner(gameBoard,row, col, marker, used_cells);   
+        console.log(winner);
     }
     else if(gameBoard[row][col] != '-'){
         console.log("not empty cell!");
-        return -1;
+        return winner -1;
     }
 }
 
@@ -47,6 +48,7 @@ function checkWinner(gameBoard,row, col,marker, used_cells){
     }
     if (flag == 3) {
         winner = marker;
+        return winner;
 
     } else { flag = 0; }
 
@@ -58,6 +60,7 @@ function checkWinner(gameBoard,row, col,marker, used_cells){
     }
     if (flag == 3) {
         winner = marker;
+        return winner;
 
     } else { flag = 0; }
 
@@ -70,6 +73,7 @@ function checkWinner(gameBoard,row, col,marker, used_cells){
     }
     if (flag == 3) {
         winner = marker;
+        return winner;
 
     } else { flag = 0; }
     //top-right
@@ -80,6 +84,7 @@ function checkWinner(gameBoard,row, col,marker, used_cells){
     }
     if (flag == 3) {
         winner = marker;
+        return winner;
 
     } else { flag = 0; }
 }
@@ -87,21 +92,25 @@ function checkWinner(gameBoard,row, col,marker, used_cells){
 if(flag!=0){console.log("Winner is" + winner);}
 else{
     if(used_cells >= 9)
-    {console.log("draw");}
+    {
+        // console.log("draw");
+        winner = "draw";
+        return winner;
+    }
 }
 }
 initializeGame();
-//draw scenario
-mark(1, 0, 0)  // X at top-left
-mark(0, 0, 1)  // O at top-middle
-mark(1, 0, 2)  // X at top-right
-mark(0, 1, 1)  // O at center
-mark(1, 1, 0)  // X at middle-left
-mark(0, 1, 2)  // O at middle-right
-mark(1, 2, 1)  // X at bottom-middle
-mark(0, 2, 0)  // O at bottom-left
-mark(1, 2, 2)  // X at bottom-right
-//x winner scenario
+// //draw scenario
+// mark(1, 0, 0)  // X at top-left
+// mark(0, 0, 1)  // O at top-middle
+// mark(1, 0, 2)  // X at top-right
+// mark(0, 1, 1)  // O at center
+// mark(1, 1, 0)  // X at middle-left
+// mark(0, 1, 2)  // O at middle-right
+// mark(1, 2, 1)  // X at bottom-middle
+// mark(0, 2, 0)  // O at bottom-left
+// mark(1, 2, 2)  // X at bottom-right
+// //x winner scenario
 // mark(1, 0, 0)  // X at top-left
 // mark(0, 0, 1)  // O at top-middle
 // mark(1, 1, 1)  // X at center
